527a2c7528cb3c0cacb65fbf62338ad0d8fd53f2
navyashreechalla <navyashreechalla@gmail.com>
2026-05-11 20:37:49 +0530

    2nd commit

---
+++ linearsearch.java
@@ -0,0 +1,13 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] arr = {1,10,5,2,5,6,7,8};
+        int target = 2;
+        for(int i=0; i<arr.length;i++){
+            if(arr[i] == target) {
+                System.out.println(i + "Element is found");
+                break;
+            }
+        }
+    }
+}
+
